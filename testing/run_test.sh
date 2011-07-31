a2724c9d071f667100462710103810e03b72169d
Thomas E. Dickey <dickey@invisible-island.net>
2011-07-31 01:01:56 -0400

    snapshot of project "tctest", label t20110730

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: run_test.sh,v 1.7 2011/07/24 20:31:24 tom Exp $
+# $Id: run_test.sh,v 1.11 2011/07/28 08:35:19 tom Exp $
 # test-script for tctest
 
 PROG="${TCTEST:-../tctest}"
@@ -9,27 +9,51 @@ export PATH
 
 unset LINES
 unset COLUMNS
-
-TERMINFO=`pwd`; export TERMINFO
-TERMPATH=`pwd`; export TERMPATH
-TERMINFO_DIRS=`pwd`; export TERMINFO_DIRS
+unset TERMCAP
 
 for name in *.tc
 do
+	# Help persuade the termcap library to look only at our file.
+	TERMINFO=`pwd`/$name
+	export TERMINFO
+
+	TERMPATH=$TERMINFO
+	export TERMINFO
+
+	TERMINFO_DIRS=$TERMINFO
+	export TERMINFO_DIRS
+
 	root=`basename $name .tc`
+	rm -f $root.all $root.std
 	echo "** $name"
-	"$PROG" -f $name -a >$root.tmp
+
+	echo
+	echo "...standard"
+	time "$PROG" -f $name -a -o $root.std
+	echo "** `egrep -v '^[#	]' $root.std |wc -l` entries, `egrep '^	' $root.std |wc -l` capabilities"
+
+	echo
+	echo "...complete"
+	time "$PROG" -b -f $name -a -o $root.all
+	echo "** `egrep -v '^[#	]' $root.all |wc -l` entries, `egrep '^	' $root.all |wc -l` capabilities"
+
+	echo
 	if test -f $root.ref
 	then
-		if cmp -s $root.ref $root.tmp
+		if cmp -s $root.ref $root.all
 		then
 			echo "...okay $name"
-			rm -f $root.tmp
+			rm -f $root.all
 		else
-			diff $root.ref $root.tmp |diffstat
+			diff -u $root.ref $root.all |diffstat
 		fi
 	else
-		mv $root.tmp $root.ref
+		cp $root.all $root.ref
 		echo "...saved $root.ref"
 	fi
+	if ! cmp -s $root.std $root.ref
+	then
+		diff -u $root.ref $root.std |diffstat
+	fi
+	rm -f $root.std
 done
